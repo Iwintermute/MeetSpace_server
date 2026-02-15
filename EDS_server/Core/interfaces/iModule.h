02d93f9c53d567ba3a031e569ea7e28a5d6a7b41
RebornedTigrex <157234124+RebornedTigrex@users.noreply.github.com>
2026-02-15 16:53:48 +0300

    Переименованы модули, исправлены некоторые пути, убираю лишние пространства имён

--- EDS_server/Core/interfaces/iModule.h
+++ EDS_server/Core/interfaces/iModule.h
@@ -2,21 +2,18 @@
 
 #include <string>
 
-namespace Sys{
+class IModule {
+public:
+    virtual ~IModule() = default;
 
-    class IModule {
-    public:
-        virtual ~IModule() = default;
+    virtual int getId() const = 0;
+    virtual std::string getName() const = 0;
 
-        virtual int getId() const = 0;
-        virtual std::string getName() const = 0;
+    // Жизненный цикл
+    virtual bool initialize() = 0;
+    virtual void shutdown() = 0;
 
-        // Жизненный цикл
-        virtual bool initialize() = 0;
-        virtual void shutdown() = 0;
-
-        // Статус
-        virtual bool isEnabled() const = 0;
-        virtual void setEnabled(bool enabled) = 0;
-    };
-}
+    // Статус
+    virtual bool isEnabled() const = 0;
+    virtual void setEnabled(bool enabled) = 0;
+};
--- EDS_server/Core/modules/BaseModule.h
+++ EDS_server/Core/modules/BaseModule.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#include "IModule.h"
+#include "interfaces/IModule.h"
 #include <atomic>
 #include <string>
 
